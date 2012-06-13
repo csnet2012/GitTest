dee9b97983e835966b8d40d92da8cf4be30d877f
Carlos Berguido <carlos.berguido@mdiachieve.com>
2012-06-13 10:06:02 -0700

    Collaborator edited their comment

--- HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
+++ HelloAndroid/src/com/example/helloandroid/HelloAndroid.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class HelloAndroid extends Activity {
     /** Called when the activity is first created. */
-	// Collaborator added this comment
+	// Collaborator edited this comment
 	// Originator edited this response comment
     @Override
     public void onCreate(Bundle savedInstanceState) {
